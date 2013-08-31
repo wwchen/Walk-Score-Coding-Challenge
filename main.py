fcf282965365ad0acb17e4e399bf76dc665fb2ab
William Chen <williamchen16@gmail.com>
2013-08-31 11:58:58 -0700

    Deriving stdtype set into a Nodeset to help keep track of nodes
    It got complicated. Node class has two list of Nodes for enter and
    leave. Graph has a dictionary of Nodes for keeping track of all the
    Nodes in the graph. List doesn't offer built in dedup, which is super
    helpful when minimizing the graph. Sets would be perfect, but it doesn't
    offer an accessor. A graph does, but it has advantages and disadvantages
    as well. In the end, I am deriving a set in a new class, Nodeset.
    Hopefully this approach will work

--- main.py
+++ main.py
@@ -12,30 +12,39 @@ except:
   print "Usage: %s filename" % sys.argv[0]
   sys.exit(1)
 
+class Nodeset(set):
+  def add(self, node):
+    if node != self:
+      return super(Nodeset, self).add(node)
+  def __getitem__(self, label):
+    for node in self:
+      if node.label == label:
+        return node
+  def __setitem__(self, label, node):
+    return super(Nodeset, self).add(node)
+
+
 class Node:
+  label = ""
   def __init__(self, label):
     self.label = label
-    self.goin = []
-    self.goout = []
+    self.leave = Nodeset()
+    self.enter = Nodeset()
 
-  def __str__(self):
+  def debug(self):
     output = "Node {} ".format(self.label)
-    output +=    "links from {} ".format(map(lambda x: x.label, self.goin))  if len(self.goin)  > 0 else ""
-    output += "and links to {}\n".format(map(lambda x: x.label, self.goout)) if len(self.goout) > 0 else ""
+    output +=   "links to {}".format(map(lambda x: x.label, self.leave)) if len(self.leave) else ""
+    output += " and " if len(self.enter) and len(self.leave)                                else ""
+    output += "links from {}".format(map(lambda x: x.label, self.enter)) if len(self.enter) else ""
+    output += "\n"
     return output
 
-  def __repr__(self):
-    return self.__str__()
-
+  def is_bridge(self):
+    return len(self.leave) == 1 and len(self.enter) == 1
+ 
   def link(self, node):
-    self.goin.append(node)
-    node.goout.append(self)
-
-class Edge:
-  def __init__(self, src, dst):
-    print "yes"
-    
-
+    self.enter.add(node)
+    node.leave.add(self)
 
 
 class Graph:
@@ -51,17 +60,45 @@ class Graph:
     self.nodes[src] = src_node
     self.nodes[dst] = dst_node
 
+  def bridge_nodes(self):
+    for node in self.nodes.values():
+      if node.is_bridge():
+        # we can assume there's only one element to pop
+        leave = node.leave.pop()
+        enter = node.enter.pop()
+        enter.leave.add(leave)
+        leave.enter.add(enter)
+        print "deleted " + node.label
+        del self.nodes[node.label]
+        del node
+    # clean up
+    for node in self.nodes.values():
+      # delete loops
+      if node in node.enter:
+        node.enter.remove(node)
+      if node in node.leave:
+        node.leave.remove(node)
+
+
+
   def __str__(self):
     output = ""
-    for node in self.nodes:
-      output += str(self.nodes[node])
+    for node in self.nodes.values():
+      output += node.debug()
+    #  output += str(self.nodes[node])
     return output
 
 
-graph = Graph()
-for line in string.split("\n"):
-  if len(line.strip()) == 0:
-    continue
-  (src,dst) = line.split("\t");
-  graph.add_edge(src, dst)
-print graph
+def main():
+  graph = Graph()
+  for line in string.split("\n"):
+    if len(line.strip()) == 0:
+      continue
+    (src,dst) = line.split("\t");
+    graph.add_edge(src, dst)
+  print graph
+  graph.bridge_nodes()
+  print graph
+
+if __name__ == "__main__":
+  main()
