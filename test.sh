e2ee0ab186968eb3b1cb3c69176c229877930e5f
William Chen <williamchen16@gmail.com>
2013-08-31 12:14:37 -0700

    Not outputting empty output files

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ for input in input/*; do
   ./main.py $input
 
   output=output/${input##*/}
-  if [[ -s $output ]]; then
+  if [[ -f $output ]]; then
     echo -e "\e[33mExpected output:\e[32m"
     cat $output 
     echo -e "\e[0m"
